52a35555df9b2941a5b21e4e26e475cc6dc33bb3
Stefan Kauerauf <mail@stefankauerauf.de>
2017-01-28 02:51:13 +0100

    fixed login and logout login mechanism is stupid (logout and login works for ip not session) implemented overview check

--- check_tp-link_easysmart.py
+++ check_tp-link_easysmart.py
@@ -29,47 +29,133 @@ from requests import ConnectionError
 from requests.auth import HTTPBasicAuth
 import argparse
 import sys
+import re
 
 CHECK_MODES = [
     'overview',
 ]
 
+NAGIOS_STATS = {
+    0: 'OK',
+    1: 'WARNING',
+    2: 'CRITICAL',
+    3: 'UNKNOWN',
+}
+
+PORT_STATES = {
+    0: 'disabled',
+    1: 'enabled',
+}
+
+LINK_STATES = {
+    0: 'down',
+    1: 'unknown',
+    2: '10MBit Half Duplex',
+    3: '10MBit Full Duplex',
+    4: '100MBit Half Duplex',
+    5: '100MBit Full Duplex',
+    6: '1000MBit Full Duplex',
+}
+
+
+
+
 class Plugin():
 
-    def __init__(self, hostaddress, username, password, mode):
+    def __init__(self, hostaddress, username, password, mode, html):
         self.hostaddress = hostaddress
         self.username = username
         self.password = password
         self.mode = mode
+        self.html = html
 
 
     def check(self):
         return getattr(self, 'check_%s' % self.mode)()
 
     def check_overview(self):
-        result_body = self.make_request('PortStatisticsRpm.htm')
+        statistics = self.get_statistics()
+        text = ''
+        perf = ''
+        for port in statistics:
+            text += 'port %d %s (%s) TX OK: %d TX ERR: %d RX OK: %d RX ERR: %d' % (port['port'],
+                                                   port['state'],
+                                                   port['link'],
+                                                   port['tx_ok'],
+                                                   port['tx_err'],
+                                                   port['rx_ok'],
+                                                   port['rx_err']
+                                                   )
+            if self.html:
+                text += '</br>'
+            else:
+                text += '\n'
+            for element in ['tx_ok', 'tx_err', 'rx_ok', 'rx_err']:
+                perf += 'port_%s_%s=%d ' % (port['port'], element, port[element])
+        text = text.strip('\n')
+        perf = perf.strip(' ')
+        return 0, text, perf
 
-        return 0, 'text', 'perf'
+    def get_statistics(self):
+        self.login()
+        statistics_body = self.make_request('PortStatisticsRpm.htm')
+        port_states = [int(i) for i in re.search('state\:\[(.*)\]\,\n', statistics_body).group(1).split(',')]
+        del port_states[-2:]
+        port_links = [int(i) for i in re.search('link_status\:\[(.*)\]\,\n', statistics_body).group(1).split(',')]
+        del port_links[-2:]
+        port_statistics = [int(i) for i in re.search('pkts\:\[(.*)\]\n', statistics_body).group(1).split(',')]
+        del port_statistics[-2:]
+        port_statistics = [port_statistics[i:i+4] for i in range(0, len(port_statistics), 4)]
+        statistics = []
+        for i in range(len(port_states)):
+            statistics.append({
+                'port': i + 1,
+                'state': PORT_STATES[port_states[i]],
+                'link': LINK_STATES[port_links[i]],
+                'tx_ok': port_statistics[i][0],
+                'tx_err': port_statistics[i][1],
+                'rx_ok': port_statistics[i][2],
+                'rx_err': port_statistics[i][3],
+            })
+        self.logout()
+        return statistics
+
+    def login(self):
+        self.session = requests.Session()
+        login_payload = {
+            'username': self.username,
+            'password': self.password,
+            'logon': 'Login',
+        }
+        try:
+            self.session.post('http://%s/logon.cgi' % self.hostaddress, data=login_payload)
+            check_login_response = self.session.get('http://%s/' % self.hostaddress)
+            if int(check_login_response.status_code) != 200:
+                print('UNKNOWN: login incorrect, check username and password')
+                sys.exit(3)
+        except ConnectionError as e:
+            print('UNKNOWN: connection error during login, check host address')
+            sys.exit(3)
+
+    def logout(self):
+        self.session.get('http://%s/Logout.htm' % self.hostaddress)
 
     def make_request(self, target=None):
         url = 'http://%s' % self.hostaddress
         if target:
             url += '/%s' % target
-        with requests.Session() as session:
-            #TODO: make fucking login first, but how?
-            try:
-                result = requests.get(url)
-                if int(result.status_code) != 200:
-                    raise Exception('unknown error, check compatibility list')
-            except ConnectionError:
-                print('UNKNOW: connection error, check host address')
-                sys.exit(3)
-            except Exception as e:
-                print('UNKNOWN: %s' % e.message)
-                sys.exit(3)
-        return result.text
 
-# ConnectionError falsche ip
+        try:
+            result = self.session.get(url)
+            if int(result.status_code) != 200:
+                raise Exception('unknown error, check compatibility list')
+        except ConnectionError as e:
+            print('UNKNOW: connection error, check host address')
+            sys.exit(3)
+        except Exception as e:
+            print('UNKNOWN: %s' % e.message)
+            sys.exit(3)
+        return result.text
 
 def main():
     '''main function configures baseic stuff, parse and check arguments and initializes check'''
@@ -78,12 +164,14 @@ def main():
     argp.add_argument('-U', '--username', help='the username of the administrative web gui user')
     argp.add_argument('-P', '--password', help='the password of the administrative web gui user')
     argp.add_argument('-M', '--mode', help='check mode, available modes are: %s' % ', '.join(CHECK_MODES))
+    argp.add_argument('--html', default=False, action='store_true', help='enable html formatted output')
     args = argp.parse_args()
 
     plugin = Plugin(hostaddress=args.hostaddress, username=args.username,
-                    password=args.password, mode=args.mode)
+                    password=args.password, mode=args.mode, html=args.html)
     result_code, result_text, result_perfdata = plugin.check()
-
+    print('%s: %s|%s' % (NAGIOS_STATS[result_code], result_text, result_perfdata))
+    sys.exit(result_code)
 
 if __name__ == '__main__':
     main()
